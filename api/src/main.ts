63083d72e5d22b42b140c986becf78e727fde456
Laio Gabriel <laiogabriel110604@gmail.com>
2026-03-30 12:29:58 -0300

    feat: initialize API project with movie data and HTTP request definitions

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {configDotenv} from "dotenv";
+import { filmes } from "./data/filmes.ts";
 
 configDotenv()
 const porta = process.env.PORTA
@@ -9,6 +10,33 @@ const app = express();
 app.listen(porta, () => {
     console.log(`Server is running on port ${porta}!!`);
 });
+
+app.use((req, res, next) => {
+    console.log("Query params:", req.query);
+    next();
+});
 app.get("/ping", (req, res) => {
-    res.send("Hello World!");
+    res.send("Pong!");
+});
+
+app.get("/filmes", (req, res) => {
+    res.json(filmes);
+});
+
+app.get("/filmes/:id", (req, res) => {
+    const id = req.params.id;
+    const { ignorar } = req.query;
+    const filme = filmes.find((filme) => filme.id === id);
+    if (!filme) {
+        return res.status(404).send("Filme não encontrado");
+    }
+
+    const copia = { ...filme } as any;
+    if (typeof ignorar === "string") {
+        ignorar.split(",").forEach((campo) => {
+            delete copia[campo.trim()];
+        });
+    }
+
+    res.json(copia);
 });
